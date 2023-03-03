ee0b8a59141433a3dbb3720b163783c022646aa7
ahmadhamzh <ahmadhamzh@ymail.com>
2023-03-03 08:52:38 +0300

    add Dockerfile

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", handlers.MainRouterPathHandeler).Methods("get")
-	router.HandleFunc("/addTeacher", handlers.AddTeacher).Methods("get")
+	router.HandleFunc("/addTeacher", handlers.AddTeacher).Methods("post")
 
 	http.ListenAndServe(":8080", router)
 }
